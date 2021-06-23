f0d72384fbd6ebdb6b2987d6cfa9e5e22860f0ea
lindabandelow <lindabandelow@yahoo.de>
2021-06-23 12:10:09 +0200

    changfjdkjak

---
+++ kurs.py
@@ -0,0 +1,19 @@
+y = 8
+print(y)
+
+
+import pandas as pd
+
+d = {"column1":1, "column2": "string"}
+df = pd. DataFrame.from_dict(d, orient="index")
+
+
+pd.DataFrame()
+
+
+x = "90"
+
+y = 100
+z = x+y
+
+j = h
